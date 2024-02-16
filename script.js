62d53735d2d8adf743f86010ce3c56c7717b1a78
NikhilPeddireddy <139708292+NikhilPeddireddy@users.noreply.github.com>
2024-02-17 00:28:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,6 @@
             let radius = prompt("Enter the radius of the circle:");
             // write you code here and display the result to the user
                     let radius = prompt("Enter the radius of the circle:");
-            if (isNaN(radius) || radius <= 0) {
-    console.log("Please enter a valid positive number for the radius.");
-} else {
     const area = Math.PI * radius * radius;
 
     const roundedArea = area.toFixed(2);
